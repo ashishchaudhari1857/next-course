902e9b462e2dbf23ccffabe3592aecdec0736145
Ashish Chaudhari <ashishchaudharistudy1857@gmail.com>
2023-07-17 12:28:57 +0530

    nested routing

---
+++ pages/about/developer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Developer=() => {
+  return (
+    <div>
+      {/* Component JSX */}
+      <h1>iam a developer</h1>
+    </div>
+  );
+};
+
+export default Developer;
