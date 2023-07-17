de29e0e1b1d2bcba232152d717ffc4b468877440
Ashish Chaudhari <ashishchaudharistudy1857@gmail.com>
2023-07-17 14:19:54 +0530

    dynamic  routing

--- pages/about/[role].js
+++ pages/about/[role].js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+ import { useRouter } from 'next/router';
 const Developer=() => {
+  const router=useRouter();
+  const name =router.query.role;
   return (
     <div>
       {/* Component JSX */}
-      <h1>iam a developer</h1>
+      <h1>iam a developer{name}</h1>
     </div>
   );
 };
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
-
+import Link from 'next/link';
+ 
 const About = () => {
+
+  const details = [
+
+    { id : 1, name: 'Yash', role: 'Senior Developer'},
+    
+    { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
+    
+    { id : 3, name: 'Suresh', role: 'Frontend Developer'}
+    
+    ]
   return (
     <div>
       {/* Component JSX */}
       <h1>this is about page</h1>
+
+   {
+    details.map((item)=>(
+      <Link  href={`/about/${item.id}`}>
+       go</Link>
+    ))
+      
+   }
     </div>
   );
 };
 
 export default About;
+
+
